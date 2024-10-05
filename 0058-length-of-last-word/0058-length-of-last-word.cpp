4312e4e31c953d26f4ec4408e0a4a384631b3425
umar-dev <umarmd2k22@gmail.com>
2024-10-05 13:54:24 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (86.85%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        if(s.size()==NULL){
+            return 0;
+        }
+        istringstream stream(s);
+        while(stream>>s){
+            s.length();
+        }
+    return s.length();
+    }
+};
