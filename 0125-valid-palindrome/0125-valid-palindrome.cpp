298776827deca66df5af0513d17b48e007937e74
umar-dev <umarmd2k22@gmail.com>
2024-11-16 12:49:34 +0530

    Time: 4 ms (16.11%), Space: 9 MB (52.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.size() - 1;
-
-        while (left < right) {
-            // Skip non-alphanumeric characters from the left
-            while (left < right && !isalnum(s[left])) ++left;
-
-            // Skip non-alphanumeric characters from the right
-            while (left < right && !isalnum(s[right])) --right;
-
-            // Compare characters case-insensitively
-            if (tolower(s[left]) != tolower(s[right])) {
+        int left=0,right=s.size()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left])) ++left;
+            while(left<right && !isalnum(s[right])) --right;
+            if(tolower(s[left])!=tolower(s[right])){
                 return false;
             }
-
-            // Move pointers inward
             ++left;
             --right;
         }
